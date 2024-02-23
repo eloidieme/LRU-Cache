6c8029dacd6691a7e076da10e95b979aacf89781
Eloi Dieme <eloidieme@gmail.com>
2024-02-23 05:14:57 +0100

    updated readme

--- src/main.c
+++ src/main.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
-
 #include "dll.h"
 #include "hashtable.h"
 
-int main(int argc, char** argv) {
-  char* prep[] = {"Félicité", "Emmanuel"};
-  char* app[] = {"Fidèle", "Marie Thérèse"};
-  dll* family = dllCreate("Eloi", 2, 2, prep, app);
-  dll* eloi = dllFind(family, "Eloi");
+int main(int argc, char **argv) {
+  char *prep[] = {"Félicité", "Emmanuel"};
+  char *app[] = {"Fidèle", "Marie Thérèse"};
+  dll *family = dllCreate("Eloi", 2, 2, prep, app);
+  dll *eloi = dllFind(family, "Eloi");
   dllInsertAfter(family, eloi, "Mamingue");
   dllInsertBefore(family, eloi, "Apache");
   dllPrint(family);
-  tableNode** hashT = hashTableInit(dllLength(family));
+  tableNode **hashT = hashTableInit(dllLength(family));
   hashTableFillFromDll(hashT, family);
   hashTablePrint(hashT, dllLength(family));
   hashTableDestroy(hashT);
